e02409ddd3983da0c4b847ca92d6354e5f20b8ea
Crazerr12 <anton_fadeev_2005@mail.ru>
2022-04-10 07:58:07 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,32 @@
+interface Publication {
+    val price : Int
+    val wordCount : Int
+    fun getType (): String
+}
+
 fun main(args: Array<String>) {
+    buy(publication = Publication)
 
-    data class User(var name: String, val surName: String, var age: Int)
-    var y = 0
-    var count = 10
-    var x = 0
-
-    var peoples = mutableListOf<User>(
-        User("namePeople", "surnamePeople", 10),
-        User("namePeople", "surnamePeople", 10),
-        User("namePeople", "surnamePeople", 10),
-        User("namePeople", "surnamePeople", 10)
-    )
-
-    while(y!=1) {
-        println("Здравствуйте, чтобы пройти в торговый центр вам нужно указать свои контактные данные:")
-
-        print("Имя:")
-        val namePeople = readLine().toString()
-
-        print("Фамилия:")
-        val surnamePeople = readLine().toString()
-
-        print("Возраст:")
-        var agePeople = readLine()!!.toInt()
-
-        println("$namePeople $surnamePeople: вы можете пройти пройти в торговый центр")
-
-        val people = User(namePeople, surnamePeople, agePeople)
-
-        peoples[x]=people
-
-        println("0 - продолжить работу")
-        println("1 - завершить работу")
-
-        y = readLine()!!.toInt()
-        x++
+    abstract class Book : Publication {
+        val page1: String? = null
+        val page2: String? = "Hello"
+        override fun getType(): String {
+            return if (wordCount<=1000) {
+                ("Flash Fiction")
+            } else{if (wordCount<=7500) {
+                ("Short Story")
+            } else ("Novel")
+            }
+        }
     }
-    peoples.sortBy { it.surName }
 
-    peoples.forEach{
-        println("name: ${it.name}, surName: ${it.surName}, Age: ${it.age}")
+    abstract class Magazine: Publication {
+        override fun getType(): String {
+            return "Magazine"
+        }
     }
+}
+
+fun buy(publication: String) {
+    println("The purchase is complete. The purchase amount was ${Publication.price}")
 }
